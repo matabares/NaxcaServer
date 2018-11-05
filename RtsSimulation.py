3fa0fe5e34c0c45193137474677b93e302250270
matabares <matabares@gmail.com>
2018-11-05 12:21:53 -0500

    Adicion de 1DayStay1Room1Adt para rts

--- RtsSimulation.py
+++ RtsSimulation.py
@@ -2,29 +2,17 @@ class RtsSimulation:
 
     def RtsResponse(self, info):
         info.send_response(200)
-        info.send_header('Content-Type', 'text/xml')
+        info.send_header('Content-Type', 'text/xml;charset=UTF-8')
         info.end_headers()
         contentLen = int(info.headers['Content-Length'])
         postBody = info.rfile.read(contentLen)
         body = str(postBody, "utf-8")
-        print(body)
-        print(info.path)
-
-
-        if "NDC_AirShopping_16.1" in info.path:
-
-            if "<Date>2030-01-01</Date>" in body:
-                file = open("providersimulation/avianca/Domestic1AdtOW.xml",
-                            "r", encoding='utf8')
-                data = file.read()
-                file.close()
-                info.wfile.write(bytes(data, 'UTF-8'))
-                print(data)
-                return info
-            if "<Date>2030-01-02</Date>" in body:
-                file = open("providersimulation/avianca/Domestic1AdtRT.xml",
-                            "r", encoding='utf8')
+        if "GetHotelSearchListForCustomerCount" in body:
+            if "<CheckInDate>2030-01-01</CheckInDate>" in body:
+                file = open("providersimulation/rts/1DayStay1Room1Adt.xml",
+                        "r", encoding='utf8')
                 data = file.read()
                 file.close()
                 info.wfile.write(bytes(data, 'UTF-8'))
                 return info
+
