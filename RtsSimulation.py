bae05e4636b06011d2ec2208ae87d145d62be2b0
danOspin <cdaniel94@gmail.com>
2018-11-08 15:42:17 -0500

    Se adicionan tramas para el Search y se actualizan códigos en tramas del Info
    Se adicionan tramas para el Search y se actualizan códigos en tramas del Info

--- RtsSimulation.py
+++ RtsSimulation.py
@@ -15,7 +15,60 @@ class RtsSimulation:
                 file.close()
                 info.wfile.write(bytes(data, 'UTF-8'))
                 return info
-                
+
+            elif "<CheckInDate>2019-01-01</CheckInDate>" in body:
+                file = open("providersimulation/rts/HotelSearch_3DayStay1Room1Adt.xml",
+                        "r", encoding='utf8')
+                data = file.read()
+                file.close()
+                info.wfile.write(bytes(data, 'UTF-8'))
+                return info
+
+            elif "<CheckInDate>2020-01-01</CheckInDate>" in body:
+                file = open("providersimulation/rts/HotelSearch_3DayStay1Room1Adt_2Room2Adt1Chd.xml",
+                        "r", encoding='utf8')
+                data = file.read()
+                file.close()
+                info.wfile.write(bytes(data, 'UTF-8'))
+                return info
+
+            elif "<CheckInDate>2030-01-01</CheckInDate>" in body:
+                file = open("providersimulation/rts/HotelSearch_3DayStay1Room2Adt_2Room2Adt.xml",
+                        "r", encoding='utf8')
+                data = file.read()
+                file.close()
+                info.wfile.write(bytes(data, 'UTF-8'))
+                return info
+
+            elif "<CheckInDate>2040-01-01</CheckInDate>" in body:
+                file = open("providersimulation/rts/HotelSearch_3DayStay1Room2Adt1Chd.xml",
+                        "r", encoding='utf8')
+                data = file.read()
+                file.close()
+                info.wfile.write(bytes(data, 'UTF-8'))
+                return info
+            elif "<CheckInDate>2050-01-01</CheckInDate>" in body:
+                file = open("providersimulation/rts/HotelSearch_3DayStay1Room2Adt1Inf.xml",
+                            "r", encoding='utf8')
+                data = file.read()
+                file.close()
+                info.wfile.write(bytes(data, 'UTF-8'))
+                return info
+            elif "<CheckInDate>2060-01-01</CheckInDate>" in body:
+                file = open("providersimulation/rts/singleHotelSearch1adtBKK0001.xml",
+                            "r", encoding='utf8')
+                data = file.read()
+                file.close()
+                info.wfile.write(bytes(data, 'UTF-8'))
+                return info
+            elif "<CheckInDate>1492-10-12</CheckInDate>" in body:
+                file = open("providersimulation/rts/singleHotelSearch1adtBKK0001.xml",
+                            "r", encoding='utf8')
+                data = file.read()
+                file.close()
+                info.wfile.write(bytes(data, 'UTF-8'))
+                return info
+
         if "GetHotelInformation" in body:
             if "<ItemCode>BKK0001</ItemCode>" in body and "<LanguageCode>AR</LanguageCode>" in body:
                 file = open("providersimulation/rts/HotelInfoEs.xml",
