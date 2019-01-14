961667982fff4611292e0fc7d59f531019bdad3a
nafhta <nafhta@gmail.com>
2019-01-14 18:09:29 -0500

    hteldo

--- HotelDoSimulation.py
+++ HotelDoSimulation.py
@@ -7,11 +7,8 @@ class HotelDoSimulation:
         info.send_response(200)
         info.send_header('Content-Type', 'application/xml; charset=utf-8')
         info.end_headers()
-
         file = open("providersimulation/hoteldo/hoteldo.xml","r", encoding='utf8')
         data = file.read()
         file.close()
         info.wfile.write(bytes(data, 'UTF-8'))
         return info
-
-            
