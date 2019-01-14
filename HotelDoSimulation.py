b2c5cb0c41ae8831286e2c0e0b857cb6794eadcb
nafhta <nafhta@gmail.com>
2019-01-14 17:20:35 -0500

    update hoteldo

--- HotelDoSimulation.py
+++ HotelDoSimulation.py
@@ -5,7 +5,7 @@ class HotelDoSimulation:
 
     def HotelDoResponse(self, info):
         info.send_response(200)
-        info.send_header('Content-Type', 'application/json; charset=utf-8')
+        info.send_header('Content-Type', 'application/xml; charset=utf-8')
         info.end_headers()
 
         file = open("providersimulation/hoteldo/hoteldo.xml","r", encoding='utf8')
