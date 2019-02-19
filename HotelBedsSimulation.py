7fca6edf4684b5b59c0c3a2c82f238de33ec8ed3
danOspin <cdaniel94@gmail.com>
2019-02-19 15:16:58 -0500

    casos para HotelBeds
    casos para HotelBeds

--- HotelBedsSimulation.py
+++ HotelBedsSimulation.py
@@ -9,15 +9,45 @@ class HotelBedsSimulation:
         body = str(postBody, "utf-8")
 
         if "HotelValuedAvailRQ" in body:
-                file = open("providersimulation/hotelBeds/search.xml","r", encoding='utf8')
+            if "MDE" in body:
+                file = open("providersimulation/hotelBeds/Search 113715943-808B5D-DFROM_20190301_DTO_20190305_CTO_MDE_HotelValuedAvailRS.xml","r", encoding='utf8')
                 data = file.read()
                 file.close()
                 info.wfile.write(bytes(data, 'UTF-8'))
                 return info
 
         if "ServiceAddRQ" in body:
-            file = open("providersimulation/hotelBeds/verify.xml", "r", encoding='utf8')
-            data = file.read()
-            file.close()
-            info.wfile.write(bytes(data, 'UTF-8'))
-            return info
+            if "MIA" in body:
+                file = open("providersimulation/hotelBeds/Tarifa mas alta 113933827-F87ED6-DFROM_20190301_DTO_20190305_CTO_MDE_ServiceAddRS.xml", "r", encoding='utf8')
+                data = file.read()
+                file.close()
+                info.wfile.write(bytes(data, 'UTF-8'))
+                return info
+
+            if "PAR" in body:
+                file = open("providersimulation/hotelBeds/Cambio de tarifa y descuento 113933827-F87ED6-DFROM_20190301_DTO_20190305_CTO_MDE_ServiceAddRS.xml", "r", encoding='utf8')
+                data = file.read()
+                file.close()
+                info.wfile.write(bytes(data, 'UTF-8'))
+                return info
+
+            if "BOG" in body:
+                file = open("providersimulation/hotelBeds/Cambio de tarifa 113933827-F87ED6-DFROM_20190301_DTO_20190305_CTO_MDE_ServiceAddRS.xml", "r", encoding='utf8')
+                data = file.read()
+                file.close()
+                info.wfile.write(bytes(data, 'UTF-8'))
+                return info
+
+            if "ISA" in body:
+                file = open("providersimulation/hotelBeds/Cambio de políticas 113933827-F87ED6-DFROM_20190301_DTO_20190305_CTO_MDE_ServiceAddRS.xml", "r", encoding='utf8')
+                data = file.read()
+                file.close()
+                info.wfile.write(bytes(data, 'UTF-8'))
+                return info
+
+            if "ADZ" in body:
+                file = open("providersimulation/hotelBeds/Cambio de descuento 113933827-F87ED6-DFROM_20190301_DTO_20190305_CTO_MDE_ServiceAddRS.xml", "r", encoding='utf8')
+                data = file.read()
+                file.close()
+                info.wfile.write(bytes(data, 'UTF-8'))
+                return info
